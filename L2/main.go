298b1e23c232912d8125a4d2356fe714e4c1155f
Mohamed El-Refai <elrefai99@gmail.com>
2026-04-15 08:53:20 +0200

    chore: add fmt print types

--- L2/main.go
+++ L2/main.go
@@ -6,5 +6,11 @@ func main() {
 	const x string = "Hi Mohammed"
 	const y uint8 = 255
 	z := 5
+	ton_number := 9.9652424523
 	fmt.Println(x, y, z)
+	fmt.Printf("type of x is %T", x)  // type
+	fmt.Printf("\n %v", y)            // value
+	fmt.Printf("\n %e", ton_number)   // ton of different decimal number
+	fmt.Printf("\n %b", z)            // binary
+	fmt.Printf("\n %.2f", ton_number) // get 2 number of float
 }
