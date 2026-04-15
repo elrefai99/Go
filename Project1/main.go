3ed81bfcd47949e9dd2f3b97b3c7a3cb9b3e6090
Mohamed El-Refai <elrefai99@gmail.com>
2026-04-15 16:23:19 +0200

    feat: enhance main.go to include command-line flags for sorting algorithms

--- Project1/main.go
+++ Project1/main.go
@@ -2,15 +2,25 @@ package main
 
 import (
 	"Project1/sort"
+	"flag"
 	"fmt"
 )
 
 func main() {
 	y := []int{1, 4, 55, 3, 66, 7, 442, 24, 546, 7, 864}
 
-	BubbleSort := sort.BubbleSort(y)
-	BubbleSort_2 := sort.BubbleSort_2(y)
+	BubbleSort := flag.Bool("bubble1", false, "run bubble sort 1")
+	BubbleSort_2 := flag.Bool("bubble2", false, "run bubble sort 2")
 
-	fmt.Println("BubbleSort", BubbleSort)
-	fmt.Println("BubbleSort2", BubbleSort_2)
+	flag.Parse()
+
+	if *BubbleSort {
+		sort.BubbleSort(y)
+		fmt.Println("BubbleSort1:", y)
+	}
+
+	if *BubbleSort_2 {
+		sort.BubbleSort_2(y)
+		fmt.Println("BubbleSort2:", y)
+	}
 }
