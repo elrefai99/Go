7dbabbe6657b7779344d39279031164047cf8cad
Mohamed El-Refai <elrefai99@gmail.com>
2026-04-23 13:22:33 +0200

    feat: update create command to generate timestamped SQL migration files and remove redundant data file

--- L13/cmd/create.go
+++ L13/cmd/create.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"strconv"
+	"time"
 
 	"github.com/spf13/cobra"
 )
@@ -19,23 +21,19 @@ func CreateCommands() *cobra.Command {
 		Short: "Create new file",
 		Args:  cobra.ExactArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
-			var fileType string
-			fmt.Println("What file type u need (yml, sql, json)")
-			fmt.Scanln(&fileType)
-			path := "data/" + args[0] + "." + fileType
+			t := time.Now().Unix() * 1000
+			path := "data/" + "migrate-" + time.Now().String() + "-" + strconv.FormatInt(t, 10) + ".sql"
 			var data any
 
-			if fileType == "sql" {
-				jsonData, err := json.Marshal(data)
-				if err != nil {
-					fmt.Fprintf(os.Stderr, "failed to encode json data: %v\n", err)
-					return
-				}
+			jsonData, err := json.Marshal(data)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "failed to encode json data: %v\n", err)
+				return
+			}
 
-				if err := os.WriteFile(path, jsonData, 0644); err != nil {
-					fmt.Fprintf(os.Stderr, "failed to write file %q: %v\n", path, err)
-					return
-				}
+			if err := os.WriteFile(path, jsonData, 0644); err != nil {
+				fmt.Fprintf(os.Stderr, "failed to write file %q: %v\n", path, err)
+				return
 			}
 		},
 	}
